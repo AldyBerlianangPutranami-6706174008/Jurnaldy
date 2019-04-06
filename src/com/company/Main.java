b477cb57889b53960a7653aff9c04f765a0870d3
Aldy Berlianang Putranami <aldybp@student.telkomuniversity.ac.id>
2019-04-06 10:57:49 +0700

    master

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner aldy = new Scanner(System.in);
+
+        System.out.print("Input : ");
+        int input = aldy.nextInt();
+        System.out.println();
+
+        System.out.println("Output : ");
+
+        for (int i = 1; i <= input; i++) {
+            int x = cari(i);
+            System.out.print(x + " ");
+        }
+
+    }
+
+    public static int cari(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return cari(n - 1) + cari(n - 2);
+        }
+    }
+
+}
